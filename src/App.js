60dd1c637c499f4f4f9b3371ddae7f95b60e35b2
Austin Perrine <akperrine@gmail.com>
2022-07-31 21:21:19 -0600

    styling of all save bullet list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-  return <div className="app-container"></div>;
+  return (
+    <div className="app-container">
+      <nav className="nav-container">
+        <div className="counter-element"></div>
+        <h1 className="app-title">To Do List</h1>
+        <button className="home-btn">
+          <FontAwesomeIcon className="fa-home" icon={faHome} />
+        </button>
+      </nav>
+      <div className="list-container">
+        <h4 className="list-title">Project 1</h4>
+        <ul className="list">
+          <li className="list item">Complete CSS</li>
+          <li className="list item">Build Components</li>
+          <li className="list item">Enjoy JS</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
