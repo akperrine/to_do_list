fa7d99eae4809786b9b27d263a9f3e03662ecc85
Austin Perrine <akperrine@gmail.com>
2022-08-01 21:15:40 -0600

    new note button, complete toggle buttons and finishing touches on original CSS draft complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faFile } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="app-container">
       <nav className="nav-container">
-        <div className="counter-element"></div>
+        <div className="counter-box">
+          <div className="counter-element"></div>
+          <div className="counter-element"></div>
+        </div>
         <h1 className="app-title">To Do List</h1>
-        <button className="home-btn">
-          <FontAwesomeIcon className="fa-home" icon={faHome} />
-        </button>
+        <div className="nav-btn-container">
+          <button className="nav-btn home-btn">
+            <FontAwesomeIcon className="fa" icon={faHome} />
+          </button>
+          <button className="nav-btn note-btn">
+            <FontAwesomeIcon className="fa" icon={faFile} />
+          </button>
+        </div>
       </nav>
       <div className="list-container">
         <h4 className="list-title">Project 1</h4>
         <ul className="list">
-          <li className="list item">Complete CSS</li>
-          <li className="list item">Build Components</li>
-          <li className="list item">Enjoy JS</li>
+          <li className="list-item">
+            <button className="task-btn todo-btn"></button>Complete CSS
+          </li>
+          <li className="list-item">
+            <button className="task-btn complete-btn"></button>Build Components
+          </li>
         </ul>
       </div>
     </div>
